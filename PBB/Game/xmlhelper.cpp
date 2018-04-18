10d22b7822ad839af1c11e189efe56b77354ab77
Seth Ballantyne <seth.ballantyne@gmail.com>
2018-04-18 18:38:32 +1200

    Implemented XmlHelper.

---
+++ PBB/Game/xmlhelper.cpp
@@ -0,0 +1,94 @@
+// Copyright(c) 2018 Seth Ballantyne <seth.ballantyne@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files(the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and / or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+#include "xmlhelper.h"
+
+String ^XmlHelper::GetAttributeValue(XElement ^element, String ^attribute)
+{
+    if(nullptr == element)
+    {
+        throw gcnew ArgumentNullException("element");
+    }
+    else if(nullptr == attribute)
+    {
+        throw gcnew ArgumentNullException("attribute");
+    }
+
+    if(String::Empty == attribute)
+    {
+        throw gcnew ArgumentException("attribute evaluates to String::Empty.");
+    }
+
+    XAttribute ^xAttribute = element->Attribute(attribute);
+    if(nullptr == xAttribute)
+    {
+        throw gcnew XmlException(String::Format("the {0} attribute is missing.", attribute));
+    }
+    
+    if(String::Empty == xAttribute->Value)
+    {
+        throw gcnew XmlException(String::Format("the value of the {0} attribute evaluates to String::Empty."));
+    }
+    else
+    {
+        return xAttribute->Value;
+    }
+}
+
+int XmlHelper::GetAttributeValueAsInt32(XElement ^element, String ^attribute)
+{
+    int number;
+    try
+    {
+        String ^initialValue = XmlHelper::GetAttributeValue(element, attribute);
+        number = Convert::ToInt32(initialValue);
+    }
+    catch(FormatException ^)
+    {
+        throw gcnew FormatException(String::Format("the {0} attribute contains a non-numeric value.", attribute));
+    }
+    catch(OverflowException ^)
+    {
+        throw gcnew OverflowException(String::Format("the {0} attribute contains a number that's greater than {1}", attribute, Int32::MaxValue));
+    }
+    catch(...)
+    {
+        throw;
+    }
+}
+
+double XmlHelper::GetAttributeAsDouble(XElement ^element, String ^attribute)
+{
+    try
+    {
+        String ^initialValue = XmlHelper::GetAttributeValue(element, attribute);
+        return Convert::ToDouble(initialValue);
+    }
+    catch(FormatException ^)
+    {
+        throw gcnew FormatException(String::Format("the {0} attribute contains a non-numeric value.", attribute));
+    }
+    catch(OverflowException ^)
+    {
+        throw gcnew OverflowException(String::Format("the attribute {0} contains a "
+            "value that is less than {1} or greater than {2}.", attribute, Double::MinValue, Double::MaxValue));
+    }
+    catch(...)
+    {
+        throw;
+    }
+}
---
+++ PBB/Game/xmlhelper.h
@@ -0,0 +1,88 @@
+// Copyright(c) 2018 Seth Ballantyne <seth.ballantyne@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files(the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and / or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+#pragma once
+
+using namespace System;
+using namespace System::Xml;
+using namespace System::Xml::Linq;
+
+/// <summary>
+/// Helper class that retrieves the values of the specified XML attribute.
+/// </summary>
+public ref class XmlHelper abstract sealed
+{
+public:
+    /// <summary>
+    /// Gets the value of the specified attribute. 
+    /// </summary>
+    /// <param name="element">the element the attribute belongs to.</param>
+    /// <param name="attribute">the desired attribute to query.</param>
+    /// <returns>the value of the attribute as a string.</returns>
+    /// <exception cref="System::ArgumentException"><i>attribute</i> evaluates to String::Empty.</exception>
+    /// <exception cref="System::ArgumentNullException"><i>element</i> or <i>attribute</i> evaluates to <b>null</b>.</exception>
+    /// <exception cref="System::Xml::XmlException">the specified attribute is missing from the element or its value evaluates to String::Empty.</exception>
+    static String ^GetAttributeValue(XElement ^element, String ^attribute);
+
+    /// <summary>
+    /// Gets the value of the specified attribute. 
+    /// </summary>
+    /// <param name="element">the element the attribute belongs to.</param>
+    /// <param name="attribute">the desired attribute to query.</param>
+    /// <returns>the attributes value as an integer.</returns>
+    /// <exception cref="System::ArgumentException"><i>attribute</i> evaluates to String::Empty.</exception>
+    /// <exception cref="System::ArgumentNullException"><i>element</i> or <i>attribute</i> evaluates to <b>null</b>.</exception>
+    /// <exception cref="System::FormatException">the attributes value is non-numeric.</exception>
+    /// <exception cref="System::OverflowException">the attributes value is greater than Int32::MaxValue.</exception>
+    /// <exception cref="System::Xml::XmlException">the specified attribute is missing from the element or its value evaluates to String::Empty.</exception>
+    static int GetAttributeValueAsInt32(XElement ^element, String ^attribute);
+
+    /// <summary>
+    /// Gets the value of the specified attribute. 
+    /// </summary>
+    /// <param name="element">the element the attribute belongs to.</param>
+    /// <param name="attribute">the desired attribute to query.</param>
+    /// <returns>the boolean value of the attribute.</returns>
+    /// <exception cref="System::ArgumentException"><i>attribute</i> evaluates to String::Empty.</exception>
+    /// <exception cref="System::ArgumentNullException"><i>element</i> or <i>attribute</i> evaluates to <b>null</b>.</exception>
+    /// <exception cref="System::Xml::XmlException">the specified attribute is missing from the element or its value evaluates to String::Empty.</exception>
+    static bool GetAttributeAsBoolean(XElement ^element, String ^attribute)
+    {
+        try
+        {
+            String ^temp = XmlHelper::GetAttributeValue(element, attribute);
+            return Convert::ToBoolean(temp);
+        }
+        catch(...)
+        {
+            throw;
+        }
+    }
+
+    /// <summary>
+    /// Gets the value of the specified attribute. 
+    /// </summary>
+    /// <param name="element">the element the attribute belongs to.</param>
+    /// <param name="attribute">the desired attribute to query.</param>
+    /// <returns>the double value of the attribute.</returns>
+    /// <exception cref="System::ArgumentException"><i>attribute</i> evaluates to String::Empty.</exception>
+    /// <exception cref="System::ArgumentNullException"><i>element</i> or <i>attribute</i> evaluates to <b>null</b>.</exception>
+    /// <exception cref="System::FormatException">the attributes value is non-numeric.</exception>
+    /// <exception cref="System::OverflowException">the attributes value is greater than Double::MaxValue or less than Double::MinValue.</exception>
+    /// <exception cref="System::Xml::XmlException">the specified attribute is missing from the element or its value evaluates to String::Empty.</exception>
+    static double GetAttributeAsDouble(XElement ^element, String ^attribute);
+};
