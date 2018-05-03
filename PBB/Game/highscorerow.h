c7ef0c31f666f23d83849ef9b771e7da11b2caa4
Seth Ballantyne <seth.ballantyne@gmail.com>
2018-05-04 08:49:09 +1200

    Documentation

--- PBB/Game/highscorerow.h
+++ PBB/Game/highscorerow.h
@@ -30,9 +30,46 @@ private:
     Label ^playerNameLabel;
     Label ^scoreLabel;
 public:
+    /// <summary>
+    /// Initialises a new instance of HighScoreRow.
+    /// </summary>
+    /// <param name="rowNumberFont">the name of the font that will be used to render the rank text.</param>
+    /// <param name="indexPosition">the screen position where the rank should be rendered.</param>
+    /// <param name="playerNameFont">the name of the font that will be used to render the players name.</param>
+    /// <param name="playerNamePosition">the screen position where the players name should be rendered.</param>
+    /// <param name="scoreFont">the name of the font that will be used to render the players score.</param>
+    /// <param name="scorePosition">the screen position where the score should be rendered.</param>
+    /// <exception cref="System::ArgumentException">either <i>rowNumberFont</i>, or <i>playerNameFont</i>, 
+    /// or <i>scoreFont</i> evaluate to String::Empty.</exception>
+    /// <exception cref="System::ArgumentNullException">either <i>rowNumberFont</i>, or <i>playerNameFont</i>, 
+    /// or <i>scoreFont</i> are <b>null</b>.</exception>
+    /// <exception cref="ResourceNotFoundException">the font specified in either <i>rowNumberFont</i>, or <i>playerNameFont</i>, 
+    /// or <i>scoreFont</i> doesn't exist within the resource manager.</exception>
     HighScoreRow(String ^rowNumberFont, Point ^indexPosition, String ^playerNameFont,
         Point ^playerNamePosition, String ^scoreFont, Point ^scorePosition);
 
+    /// <summary>
+    /// Blits the rank, players name and score to the backbuffer.
+    /// </summary>
+    /// <exception cref="System::Runtime::InteropServices::COMException">An unspecified COM error was returned.</exception>
+    /// <exception cref="DirectDrawGenericException">DirectDraw returned an unspecified error condition.</exception>
+    /// <exception cref="DirectDrawInvalidClipListException">DirectDraw does not support the provided clip list.</exception>
+    /// <exception cref="DirectDrawInvalidObjectException">DirectDraw received a pointer that was an invalid DirectDraw object.</exception>
+    /// <exception cref="DirectDrawInvalidParametersException">one or more of the parameters passed to the method are incorrect.</exception>
+    /// <exception cref="DirectDrawInvalidRectException">the rectangle coordinates used by the surface were invalid.</exception>
+    /// <exception cref="DirectDrawNoAlphaHardwareException">no alpha acceleration hardware is present or available.</exception>
+    /// <exception cref="DirectDrawNoBlitHardwareException">no blitter hardware is present.</exception>
+    /// <exception cref="DirectDrawNoClipListException">no clip list is available.</exception>
+    /// <exception cref="DirectDrawNoDDRasterOperationHardwareException">no DirectDraw raster operation (ROP) hardware is available.</exception>
+    /// <exception cref="DirectDrawNoMirrorHardwareException">the operation cannot be carried out because no mirroring hardware is present or available.</exception>
+    /// <exception cref="DirectDrawNoRasterOperationHardwareException">the operation cannot be carried out because no appropriate raster operation hardware is present or available.</exception>
+    /// <exception cref="DirectDrawNoRotationHardwareException">the operation cannot be carried out because no rotation hardware is present or available.</exception>
+    /// <exception cref="DirectDrawNoStretchHardwareException">the operation cannot be carried out because there is no hardware support for stretching.</exception>
+    /// <exception cref="DirectDrawNoZBufferHardwareException">the operation cannot be carried out because there is no hardware support for Z-buffers.</exception>
+    /// <exception cref="DirectDrawSurfaceBusyException">access to the surface is refused because the surface is locked by another thread.</exception>
+    /// <exception cref="DirectDrawSurfaceLostException">access to the surface is refused because the surface memory is gone.</exception>
+    /// <exception cref="DirectDrawUnsupportedException">the operation is not supported.</exception>
+    /// <exception cref="DirectDrawWasStillDrawingException">the previous blit operation is incomplete.</exception>
     void Render()
     {
         try
@@ -47,6 +84,9 @@ public:
         }
     }
 
+    /// <summary>
+    /// Gets the label used to display the scores rank on the high score table. 
+    /// </summary>
     property Label ^Index
     {
         Label ^get()
@@ -55,6 +95,9 @@ public:
         }
     }
 
+    /// <summary>
+    /// Gets the label used to display the players name on the high score table.
+    /// </summary>
     property Label ^PlayerName
     {
         Label ^get()
@@ -63,6 +106,9 @@ public:
         }
     }
 
+    /// <summary>
+    /// Gets the label used to display the score on the high score table.
+    /// </summary>
     property Label ^Score
     {
         Label ^get()
